92d8b011451343f59916c91b95f0abb1930c1eea
Nather_ / Nathan Delenclos <nathan.delenclos@epitech.eu>
2023-12-17 00:22:56 +0100

    feat: auto build and push on registry

---
+++ roles/jenkins/files/deploy.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+LANGUAGE=()
+
+if [[ -f Makefile ]]; then
+	LANGUAGE+=("c")
+fi
+if [[ -f app/pom.xml ]]; then
+	LANGUAGE+=("java")
+fi
+if [[ -f package.json ]]; then
+	LANGUAGE+=("javascript")
+fi
+if [[ -f requirements.txt ]]; then
+	LANGUAGE+=("python")
+fi
+if find app -type f | grep -q 'app/main.bf'; then
+	LANGUAGE+=("befunge")
+fi
+
+if [[ ${#LANGUAGE[@]} == 0 ]]; then
+	echo "Invalid project: no language matched."
+	exit 1
+fi
+if [[ ${#LANGUAGE[@]} != 1 ]]; then
+	echo "Invalid project: multiple languages matched (${LANGUAGE[@]})."
+	exit 1
+fi
+echo "${LANGUAGE[@]} matched"
+
+image_name="$2/whanos/whanos-$1-${LANGUAGE[0]}"
+
+if [[ -f Dockerfile ]]; then
+	docker build . -t "$image_name"
+else
+	docker build . -f "/var/lib/jenkins/images/${LANGUAGE[0]}/Dockerfile.standalone" -t "$image_name"
+fi
+
+if ! docker push "$image_name"; then
+	exit 1
+fi
---
+++ roles/jenkins/files/jobs/main.groovy
@@ -0,0 +1,58 @@
+folder("Whanos base images") {
+	description("The base images of whanos.")
+}
+
+folder("Projects") {
+	description("The available projets in whanos.")
+}
+
+languages = ["c", "java", "javascript", "python", "befunge"]
+
+languages.each { language ->
+	freeStyleJob("Whanos base images/whanos-$language") {
+		steps {
+			shell("docker build /var/lib/jenkins/images/$language -f /var/lib/jenkins/images/$language/Dockerfile.base -t whanos-$language")
+		}
+	}
+}
+
+freeStyleJob("Whanos base images/Build all base images") {
+	publishers {
+		downstream(
+			languages.collect { language -> "Whanos base images/whanos-$language" }
+		)
+	}
+}
+
+freeStyleJob("link-project") {
+	parameters {
+		stringParam("GIT_URL", null, 'Git repository url (e.g.: "https://github.com/Octopus773/ts-hello-world.git")')
+		stringParam("DISPLAY_NAME", null, "Display name for the job")
+		stringParam("REGISTRY_URL", "{{registry_domain}}", "Registry to store docker image")
+	}
+	steps {
+		dsl {
+			text('''
+				freeStyleJob("Projects/$DISPLAY_NAME") {
+					scm {
+						git {
+							remote {
+								name("origin")
+								url("$GIT_URL")
+							}
+						}
+					}
+					triggers {
+						scm("* * * * *")
+					}
+					wrappers {
+						preBuildCleanup()
+					}
+					steps {
+						shell("/var/lib/jenkins/deploy.sh \\"$DISPLAY_NAME\\" \\"$REGISTRY_URL\\"")
+					}
+				}
+			''')
+		}
+	}
+}
